6c78dae87b77303c5fb602229e5ac0a8541bb695
Palamarchuk_Tanya <t0973753805@gmail.com>
2022-08-10 21:10:18 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,15 +2,15 @@ import Player from '@vimeo/player';
 const lodash = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
-const player = new VimeoPlayer(iframe);
+const player = new Vimeo(iframe);
 
 const onPlay = function (data) {
   const time = data.seconds;
   localStorage.setItem('videoplayer-current-time', time);
 };
 
-// const currentTime = localStorage.getItem('videoplayer-current-time');
+const currentTime = localStorage.getItem('videoplayer-current-time');
 player.on('timeupdate', lodash(onPlay, 1000));
-// if (currentTime) {
-//   player.setCurrentTime(savedTime);
-// }
+if (currentTime) {
+  player.setCurrentTime(savedTime);
+}
