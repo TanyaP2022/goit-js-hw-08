711a10251111d5494eeab9cf83bb8e64bf63597e
Palamarchuk_Tanya <t0973753805@gmail.com>
2022-08-10 21:03:28 +0300

    Друге завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+const lodash = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const onPlay = function (data) {
+  const time = data.seconds;
+  localStorage.setItem('videoplayer-current-time', time);
+};
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+player.on('timeupdate', lodash(onPlay, 1000));
+if (currentTime) {
+  player.setCurrentTime(savedTime);
+}
