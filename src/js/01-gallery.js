28ec36a9b6b306ae69c23564404e18f20b14b2df
Palamarchuk_Tanya <t0973753805@gmail.com>
2022-08-10 19:19:38 +0300

    Перше завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+
+function markupGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+          <div class="gallery__item">
+            <a class="gallery__link" href='${original}'>
+            <img
+                class="gallery__image"
+                src='${preview}'
+                data-source='${original}'
+                alt='${description}'
+            />
+            </a>
+        </div>
+        `;
+    })
+    .join('');
+}
+
+gallery.insertAdjacentHTML('beforeend', markupGalleryItems(galleryItems));
+
+new SimpleLightbox('.gallery__item', {
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
