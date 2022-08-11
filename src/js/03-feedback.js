2d4bf5a6d511d1bb3a4b0e3f91ddcb50266c9333
Palamarchuk_Tanya <t0973753805@gmail.com>
2022-08-11 22:43:52 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,15 +3,12 @@ import throttle from 'lodash.throttle';
 const formEl = document.querySelector('.feedback-form');
 const inputEl = document.querySelector('input');
 const messageEl = document.querySelector('textarea');
-const emailEl = document.querySelector('email');
 const STORAGE_KEY = 'feedback-form-state';
 
-const formData = {};
-
 formEl.addEventListener('submit', onFormSubmit);
-// formEl.addEventListener('input', throttle(onTextareaInput, 500));
+inputEl.addEventListener('input', throttle(onTextareaInput, 500));
 messageEl.addEventListener('input', throttle(onTextareaInput, 500));
-// fillForm();
+fillForm();
 formEl.addEventListener('input', even => {
   formData[even.target.name] = even.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
@@ -20,16 +17,24 @@ populateTextarea();
 
 function onFormSubmit(event) {
   event.preventDefault();
+  const formData = new FormData(formEl);
+  formData.forEach((name, value) => {
+    console.log(name, value);
+  });
+  formEl.reset();
   // formData.email = inputEl.value;
   // formData.message = messageEl.value;
   // console.log(formData);
-  event.currentTarget.reset();
+  // event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
 
 function onTextareaInput(event) {
-  const message = event.target.value;
+  const message = messageEl.value;
+  const email = inputEl.value;
   localStorage.setItem(STORAGE_KEY, message);
+  localStorage.setItem(STORAGE_KEY, email);
+
   // formData[event.target.name] = event.target.value;
   // localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
@@ -37,13 +42,11 @@ function onTextareaInput(event) {
 
 function populateTextarea() {
   const valuesObject = localStorage.getItem(STORAGE_KEY);
-  // const valuesObject = JSON.parse(rawValues);
+  valuesObject = JSON.parse(valuesObject);
 
   if (valuesObject) {
-    const savedEmail = valuesObject.email;
-    inputEl.value = savedEmail;
+    inputEl.value = valuesObject.email;
 
-    const savedMessage = valuesObject.message;
-    messageEl.value = savedMessage;
+    messageEl.value = valuesObject.message;
   }
 }
