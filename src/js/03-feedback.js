a6553b177b680a1190a154eeac75218bd010faae
Palamarchuk_Tanya <t0973753805@gmail.com>
2022-08-11 22:06:35 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,33 +7,44 @@ const STORAGE_KEY = 'feedback-form-state';
 
 const formData = {};
 
-formEl.addEventListener('input', throttle(onTextareaInput, 500));
 formEl.addEventListener('submit', onFormSubmit);
-fillForm();
-
-function onTextareaInput(event) {
-  formData[event.target.name] = event.target.value;
+// formEl.addEventListener('input', throttle(onTextareaInput, 500));
+messageEl.addEventListener('input', throttle(onTextareaInput, 500));
+// fillForm();
+formEl.addEventListener('input', even => {
+  formData[even.target.name] = even.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-}
+});
+populateTextarea();
 
 function onFormSubmit(event) {
   event.preventDefault();
-  formData.email = inputEl.value;
-  formData.message = messageEl.value;
-  console.log(formData);
+  // formData.email = inputEl.value;
+  // formData.message = messageEl.value;
+  // console.log(formData);
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
 
-function fillForm() {
-  const rawValues = localStorage.getItem(STORAGE_KEY);
-  const valuesObject = JSON.parse(rawValues);
+function onTextareaInput(event) {
+  const message = event.target.value;
+  localStorage.setItem(STORAGE_KEY, message);
+  // formData[event.target.name] = event.target.value;
+  // localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+// function fillForm() {
 
-  if (valuesObject) {
-    const savedEmail = valuesObject.email;
-    inputEl.value = savedEmail;
-
-    const savedMessage = valuesObject.message;
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  // const valuesObject = JSON.parse(rawValues);
+  if (savedMessage) {
     messageEl.value = savedMessage;
   }
+  // if (valuesObject) {
+  //   const savedEmail = valuesObject.email;
+  //   inputEl.value = savedEmail;
+
+  //   const savedMessage = valuesObject.message;
+  //   messageEl.value = savedMessage;
+  // }
 }
