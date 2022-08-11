b257ddee11216441c018004d06eb27b8ca13ca6a
Palamarchuk_Tanya <t0973753805@gmail.com>
2022-08-11 22:55:09 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -17,32 +17,25 @@ populateTextarea();
 
 function onFormSubmit(event) {
   event.preventDefault();
-  const formData = new FormData(formEl);
-  formData.forEach((name, value) => {
-    console.log(name, value);
-  });
-  formEl.reset();
-  // formData.email = inputEl.value;
-  // formData.message = messageEl.value;
-  // console.log(formData);
-  // event.currentTarget.reset();
+  const formData = {};
+  // formEl.reset();
+  formData.email = inputEl.value;
+  formData.message = messageEl.value;
+  event.target.reset();
+  console.log(formData);
   localStorage.removeItem(STORAGE_KEY);
 }
 
 function onTextareaInput(event) {
-  const message = messageEl.value;
-  const email = inputEl.value;
+  const message = messageEl.target.value;
+  const email = inputEl.target.value;
   localStorage.setItem(STORAGE_KEY, message);
   localStorage.setItem(STORAGE_KEY, email);
-
-  // formData[event.target.name] = event.target.value;
-  // localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
-// function fillForm() {
 
 function populateTextarea() {
   const valuesObject = localStorage.getItem(STORAGE_KEY);
-  valuesObject = JSON.parse(valuesObject);
+  // valuesObject = JSON.parse(valuesObject);
 
   if (valuesObject) {
     inputEl.value = valuesObject.email;
