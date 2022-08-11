556d589fe2f4c1f8fbbcb09f35346eff5ef68fe2
Palamarchuk_Tanya <t0973753805@gmail.com>
2022-08-11 23:10:02 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,41 +5,43 @@ const inputEl = document.querySelector('input');
 const messageEl = document.querySelector('textarea');
 const STORAGE_KEY = 'feedback-form-state';
 
-formEl.addEventListener('submit', onFormSubmit);
 inputEl.addEventListener('input', throttle(onTextareaInput, 500));
 messageEl.addEventListener('input', throttle(onTextareaInput, 500));
-fillForm();
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(formEl);
+  formEl.forEach((name, value) => {
+    console.log(name, value);
+  });
+  formEl.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(event) {
+  const email = inputEl.value;
+  const message = messageEl.value;
+  const formData = {
+    email,
+    message,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
 formEl.addEventListener('input', even => {
   formData[even.target.name] = even.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 });
 populateTextarea();
 
-function onFormSubmit(event) {
-  event.preventDefault();
-  const formData = {};
-  // formEl.reset();
-  formData.email = inputEl.value;
-  formData.message = messageEl.value;
-  event.target.reset();
-  console.log(formData);
-  localStorage.removeItem(STORAGE_KEY);
-}
-
-function onTextareaInput(event) {
-  const message = messageEl.target.value;
-  const email = inputEl.target.value;
-  localStorage.setItem(STORAGE_KEY, message);
-  localStorage.setItem(STORAGE_KEY, email);
-}
-
 function populateTextarea() {
   const valuesObject = localStorage.getItem(STORAGE_KEY);
-  // valuesObject = JSON.parse(valuesObject);
+  valuesObject = JSON.parse(valuesObject);
 
   if (valuesObject) {
     inputEl.value = valuesObject.email;
-
     messageEl.value = valuesObject.message;
   }
 }
