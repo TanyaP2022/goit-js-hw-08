a3aa538781fb0c54c555ae44c3da80a3fe7fb6a9
Palamarchuk_Tanya <t0973753805@gmail.com>
2022-08-11 23:19:48 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,27 +1,29 @@
 import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
-const inputEl = document.querySelector('input');
+const emailEl = document.querySelector('input');
 const messageEl = document.querySelector('textarea');
+
 const STORAGE_KEY = 'feedback-form-state';
 
-inputEl.addEventListener('input', throttle(onTextareaInput, 500));
-messageEl.addEventListener('input', throttle(onTextareaInput, 500));
+emailEl.addEventListener('input', throttle(onInputChange, 500));
+messageEl.addEventListener('input', throttle(onInputChange, 500));
 
-formEl.addEventListener('submit', onFormSubmit);
+newInput();
 
-function onFormSubmit(event) {
+formEl.addEventListener('submit', event => {
   event.preventDefault();
   const formData = new FormData(formEl);
-  formEl.forEach((name, value) => {
+  formData.forEach((name, value) => {
     console.log(name, value);
   });
+
   formEl.reset();
   localStorage.removeItem(STORAGE_KEY);
-}
+});
 
-function onTextareaInput(event) {
-  const email = inputEl.value;
+function onInputChange(e) {
+  const email = emailEl.value;
   const message = messageEl.value;
   const formData = {
     email,
@@ -30,18 +32,12 @@ function onTextareaInput(event) {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-formEl.addEventListener('input', even => {
-  formData[even.target.name] = even.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-});
-populateTextarea();
+function newInput() {
+  let inputSaved = localStorage.getItem(STORAGE_KEY);
+  inputSaved = JSON.parse(inputSaved);
 
-function populateTextarea() {
-  const valuesObject = localStorage.getItem(STORAGE_KEY);
-  valuesObject = JSON.parse(valuesObject);
-
-  if (valuesObject) {
-    inputEl.value = valuesObject.email;
-    messageEl.value = valuesObject.message;
+  if (inputSaved) {
+    emailEl.value = inputSaved.email;
+    messageEl.value = inputSaved.message;
   }
 }
